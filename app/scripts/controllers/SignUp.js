463e1840dbe1041cfaa23cc0312d0c50644a7c52
liuqingx5 <liuqingx5@gmail.com>
2014-06-01 16:23:02 +0800

    judge sign up page start_end button

--- app/scripts/controllers/SignUp.js
+++ app/scripts/controllers/SignUp.js
@@ -2,25 +2,41 @@
 
 
 angular.module('myworkApp')
-    .controller('SignUpCtrl', function ($scope,$location) {
+    .controller('SignUpCtrl', function ($scope, $location) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
+        if (!Activity.start_activity()) {
+            $scope.show_which = "start";
+        } else {
+            judge_current_is_starting();
+        }
+        function judge_current_is_starting() {
+            if (Activity.current_is_starting()) {
+                $scope.show_which = "end";
+            } else {
+                $scope.show_which = "un_click";
+                $scope.disabled = true;
+            }
+        }
+
         $scope.activity_list = function () {
             $location.path('/activity_list');
         }
-        $scope.show_which="start"
-        $scope.switch_end=function(){
-            $scope.show_which="end";
+
+        $scope.switch_end = function () {
+            $scope.show_which = "end";
             Activity.change_status("start");
+            Activity.save_start_activity(Activity.current().activity);
         }
 
-        $scope.switch_start=function(){
-            $scope.show_which="start";
+        $scope.switch_start = function () {
+            $scope.show_which = "start";
             Activity.change_status("end");
+            Activity.clear_start_activity();
         }
 
 
--- app/scripts/model/ActivityModel.js
+++ app/scripts/model/ActivityModel.js
@@ -17,6 +17,12 @@ Activity.save_current_activity = function (activity) {
     localStorage.current_activity = activity;
 }
 
+Activity.current = function () {
+    return  _.find(Activity.activities(), function (activity) {
+        return activity.activity == localStorage.current_activity;
+    })
+}
+
 Activity.judge_repeat_activity = function (input_activity) {
     return  _.find(Activity.activities(), function (activity) {
         return activity.activity == input_activity;
@@ -25,15 +31,26 @@ Activity.judge_repeat_activity = function (input_activity) {
 }
 
 Activity.save_start_activity = function (name) {
-    localStorage.startint_activity = name
+    localStorage.starting_activity = name;
+}
+
+Activity.clear_start_activity = function () {
+    localStorage.starting_activity = '';
+}
+
+Activity.start_activity=function(){
+    return localStorage.starting_activity;
+}
+Activity.current_is_starting=function(){
+    return localStorage.starting_activity == localStorage.current_activity;
 }
 
 Activity.change_status = function (status) {
     var activities = JSON.parse(localStorage.getItem('activities')) || [];
     var start_activity = _.find(activities, function (activity) {
-        activity.activity = localStorage.current_activity;
+        return activity.activity == localStorage.current_activity;
     })
-    start_activity.status=status;
+    start_activity.status = status;
     localStorage.setItem('activities', JSON.stringify(activities));
 }
 
