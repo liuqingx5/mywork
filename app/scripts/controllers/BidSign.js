c774ca8d9b685219b3dfe32ceb210d18a0b0d517
liuqingx5 <liuqingx5@gmail.com>
2014-06-02 14:21:51 +0800

    change bid status

--- app/scripts/controllers/BidSign.js
+++ app/scripts/controllers/BidSign.js
@@ -1,15 +1,29 @@
 'use strict'
 
 angular.module('myworkApp')
-    .controller('BidSignCtrl', function ($scope,$location) {
+    .controller('BidSignCtrl', function ($scope, $location) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
-        $scope.go_bid_list=function (){
+        $scope.go_bid_list = function () {
             $location.path('/bid_list');
+        }
+        $scope.which_button = "start";
+
+        $scope.begin = function () {
+            $scope.which_button = "end";
+            Bid.change_status("start");
+            Bid.save_start();
+        }
+
+        $scope.end = function () {
+            if (confirm('确定要结束本次竞价吗？')) {
+                Bid.change_status("end");
+                Bid.clear_start();
+            }
         }
 
 
--- app/scripts/model/BidModel.js
+++ app/scripts/model/BidModel.js
@@ -1,7 +1,7 @@
 function Bid(activity,bid){
     this.activity=activity;
     this.name=bid;
-    this.status="un_start"
+    this.status="un_start";
 }
 
 Bid.save_bid = function () {
@@ -24,5 +24,25 @@ Bid.current_bid_lists = function () {
 }
 
 Bid.save_current_bid = function (current_bid) {
-    localStorage.setItem(('current_bid'), current_bid);
+    localStorage.current_bid=current_bid;
+}
+
+//console.log('aaa',Bid.current_bid_lists ())
+Bid.change_status = function (status) {
+    var bid_lists = JSON.parse(localStorage.getItem('bid_lists')) || [];
+    var bid_list = _.find(Bid.current_bid_lists(), function (bid_list) {
+        return bid_list.name == localStorage.current_bid
+    })
+    bid_list.status = status;
+    localStorage.setItem('bid_lists', JSON.stringify(bid_lists));
+}
+
+Bid.save_start=function(){
+    localStorage.starting_bid = localStorage.current_bid;
+    localStorage.starting_bid_avtivity = localStorage.current_activity;
+}
+
+Bid.clear_start=function(){
+    localStorage.starting_bid='';
+    localStorage.starting_bid_avtivity='';
 }
